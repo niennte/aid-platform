82b769fada5fecb7c832157d42b0836cbe1ac75c
Niennte <niennte@gmail.com>
2018-09-27 12:35:37 -0400

    Add meta tags to support responsive layout.

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -48,6 +48,7 @@ const renderApp = (
   <!doctype html>
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         ${head.title}
         ${head.meta}
         <link rel="stylesheet" href="${STATIC_PATH}/css/bootstrap.min.css">
