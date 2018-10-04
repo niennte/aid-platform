d67c1a197f71648a3f9caf2bf03b4b9d4d2e6000
Niennte <niennte@gmail.com>
2018-10-04 14:39:15 -0400

    Update rediscloud config.

--- src/shared/config-redis.js
+++ src/shared/config-redis.js
@@ -1,6 +1,8 @@
 import redis from 'redis';
 
-// const client = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true });
-const client = redis.createClient('redis://rediscloud:sa2i4e2yPzmf6bHXjUxBGpW2r9RLA9eg@redis-12376.c12.us-east-1-4.ec2.cloud.redislabs.com:12376', { no_ready_check: true });
+import isProd from './util';
+
+const redisEndpoint = isProd ? process.env.REDISCLOUD_URL : 'redis://rediscloud:sa2i4e2yPzmf6bHXjUxBGpW2r9RLA9eg@redis-12376.c12.us-east-1-4.ec2.cloud.redislabs.com:12376';
+const client = redis.createClient(redisEndpoint, { no_ready_check: true });
 
 export default client;
